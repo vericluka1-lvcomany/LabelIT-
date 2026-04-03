464436d8177e8d39e65d3130c537de3548f74789
vericluka1-lvcomany <vericluka1@gmail.com>
2026-04-03 23:51:51 +0200

    4.0

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Luka Veric-LabelIt-1.0";
+const contentToCache = [
+    "Build/Labelit2.0.loader.js",
+    "Build/Labelit2.0.framework.js",
+    "Build/Labelit2.0.data",
+    "Build/Labelit2.0.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
